c4d06a541b3438bba6cc28cccb003c4def6508ac
Ivo Branco <ivo.branco@fccn.pt>
2024-10-22 11:32:03 +0100

    fix: product title in EN
    The product titles are hardcoded in English.
    It was added a pre-save on Products to change the title
    product's to Portuguese.
    
    related to #30

--- nau_extensions/signals.py
+++ nau_extensions/signals.py
@@ -1,10 +1,14 @@
+import re
+
+from django.db.models.signals import pre_save
 from django.dispatch import receiver
 from nau_extensions.financial_manager import \
     send_to_financial_manager_if_enabled
 from nau_extensions.models import BasketTransactionIntegration
-from oscar.core.loading import get_class
+from oscar.core.loading import get_class, get_model
 
 post_checkout = get_class("checkout.signals", "post_checkout")
+Product = get_model('catalogue', 'Product')
 
 
 @receiver(
@@ -23,3 +27,23 @@ def create_and_send_basket_transaction_integration_to_financial_manager(
     bti.save()
     bti.refresh_from_db()
     send_to_financial_manager_if_enabled(bti)
+
+
+@receiver(pre_save, sender=Product)
+def change_product_titles(sender, instance, *_args, **kwargs):
+    """
+    Change Product title that is hardcoded in English.
+    So we just find and replace the English words to Portuguese on the Product title field.
+    Also remove the `verified` word, because on NAU this doesn't make sense.
+    Hardcoded code locations:
+    - https://github.com/fccn/ecommerce/blob/132e45e7af964c9addbe97deae6ab4327caa6cce/ecommerce/courses/models.py#L57
+    - https://github.com/fccn/ecommerce/blob/132e45e7af964c9addbe97deae6ab4327caa6cce/ecommerce/courses/models.py#L295
+    - https://github.com/fccn/ecommerce/blob/132e45e7af964c9addbe97deae6ab4327caa6cce/ecommerce/courses/models.py#L134
+    - https://github.com/fccn/ecommerce/blob/132e45e7af964c9addbe97deae6ab4327caa6cce/ecommerce/courses/models.py#L131
+    """
+    new_title = instance.title
+    new_title = new_title.replace('Seat in', 'Lugar em')
+    new_title = new_title.replace('Enrollment code for verified seat in', 'Código de inscrição para')
+    new_title = new_title.replace('with verified certificate', 'com certificado pago')
+    new_title = new_title.replace('with honor certificate', 'com certificado grátis')
+    instance.title = new_title
--- nau_extensions/tests/test_signals.py
+++ nau_extensions/tests/test_signals.py
@@ -1,11 +1,13 @@
-
-import mock
 from nau_extensions.models import BasketTransactionIntegration
+from oscar.core.loading import get_model
 
+from ecommerce.courses.tests.factories import CourseFactory
 from ecommerce.extensions.checkout.mixins import EdxOrderPlacementMixin
 from ecommerce.extensions.test.factories import create_order
-from ecommerce.tests.factories import UserFactory
+from ecommerce.tests.factories import PartnerFactory, UserFactory
 from ecommerce.tests.testcases import TestCase
+
+Product = get_model('catalogue', 'Product')
 
 
 class SignalsNAUExtensionsTests(TestCase):
@@ -22,3 +24,50 @@ class SignalsNAUExtensionsTests(TestCase):
         EdxOrderPlacementMixin().handle_successful_order(order)
         bti = BasketTransactionIntegration.get_by_basket(order.basket)
         self.assertTrue(bti is not None)
+
+    def test_change_product_title_verified(self):
+        """
+        Test change the product hardcoded names in english to portuguese versions for verified course run.
+        """
+        partner = PartnerFactory(short_code="edX")
+        course = CourseFactory(
+            id="course-v1:edX+DemoX+Demo_Course",
+            name="edX Demonstration Course",
+            partner=partner,
+        )
+        product = course.create_or_update_seat("verified", False, 1)
+        product.save()
+        product.refresh_from_db()
+        self.assertEqual(product.title, "Lugar em edX Demonstration Course com certificado pago")
+        self.assertEqual(product.parent.title, "Lugar em edX Demonstration Course")
+
+    def test_change_product_title_honor(self):
+        """
+        Test change the product hardcoded names in english to portuguese versions for `honor` course run.
+        """
+        partner = PartnerFactory(short_code="edX")
+        course = CourseFactory(
+            id="course-v1:edX+DemoX+Demo_Course",
+            name="edX Demonstration Course",
+            partner=partner,
+        )
+        honor_product = course.create_or_update_seat("honor", False, 0)
+        honor_product.save()
+        honor_product.refresh_from_db()
+        self.assertEqual(honor_product.title, "Lugar em edX Demonstration Course com certificado grátis")
+        self.assertEqual(honor_product.parent.title, "Lugar em edX Demonstration Course")
+
+    def test_change_product_title_enrollment_code(self):
+        """
+        Test change the product hardcoded names in english to portuguese versions for verified course run.
+        """
+        partner = PartnerFactory(short_code="edX")
+        course = CourseFactory(
+            id="course-v1:edX+DemoX+Demo_Course",
+            name="edX Demonstration Course",
+            partner=partner,
+        )
+        product = course.create_or_update_seat("verified", False, 1, create_enrollment_code=True)
+        product.save()
+        enrollment_code = course.get_enrollment_code()
+        self.assertEqual(enrollment_code.title, "Código de inscrição para edX Demonstration Course")
