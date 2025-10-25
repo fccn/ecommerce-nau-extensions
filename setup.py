ec447d10374d7eceb900f134e2884c23a655e8b4
Kevyn Suarez <kevyn.suarez@edunext.co>
2025-10-25 12:30:32 -0500

    chore: django requirement updated

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(
         'Programming Language :: Python :: 3.8',
     ],
     install_requires=[
-        'Django~=3.2',
+        'Django>=3.2',
     ],
     packages=[
         'nau_extensions',
